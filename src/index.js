827ec2ac65f089ad6d998252dc2ce64cc7e0a050
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-23 16:00:01 +0300

    feat: dog-bar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 let list = document.querySelector("#dog-bar")
-console.log(list)
 
+
+function display(data){
+   
+   console.log(data)
+    let mapper = data.map(item =>  
+        `<span>${item.name}</span>`
+        
+    ).join("")
+    
+    list.innerHTML = mapper
+    
+
+   
+}
+
+fetch("http://localhost:3000/pups")
+.then(response => response.json())
+.then(data => display(data))
