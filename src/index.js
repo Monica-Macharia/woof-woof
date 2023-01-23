09bce81b04ee8637e243a562264da4d1a47d390a
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-01-23 17:50:17 +0300

    feature: DOM

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ let list = document.querySelector("#dog-bar")
 
 function display(data){
    
-   console.log(data)
+   
     let mapper = data.map(item =>  
         `<span>${item.name}</span>`
         
@@ -11,14 +11,36 @@ function display(data){
     
     list.innerHTML = mapper
     
+        //eventlistener on the span which is a HTML collection
        let span = document.getElementsByTagName("span")
        
        for (let child of span){
-       console.log(child)
-       child.addEventListener("click", function(){
-          data.map(dog => )
+      
+       child.addEventListener("click", function(e){
+        for (let newItem of data){
+            console.log(newItem.name)
+            if(newItem.name === e.target.innerText){
+           details(newItem)
+       }
+    
+        }
+        
+      
+
        })}
    
+}
+
+function details(newItem){
+    
+    
+    let info = document.querySelector("#dog-info")
+   
+     info.innerHTML= 
+    `<img src="${newItem.image}"/>
+     <h2>${newItem.name}</h2>
+     <button id="good">Good Dog</button>`
+     
 }
 
 fetch("http://localhost:3000/pups")
